4ab6fafba7416389bfb9765ceb3b402cc108216f
loml13 <1223552894@qq.com>
2025-01-09 18:54:39 +0800

    Update menu.c

--- restaurant-system/menu.c
+++ restaurant-system/menu.c
@@ -1,6 +1,8 @@
 #include "menu.h"
 #include <stdio.h>
-#include <string.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <wchar.h>
 
 int load_menu(const char* filename, MenuItem menu[], int max_items) {
     FILE* file = fopen(filename, "r");
@@ -26,5 +28,44 @@ int load_menu(const char* filename, MenuItem menu[], int max_items) {
     return count;
 }
 
+int add_menu(const char* filename)
+{
+    // setlocale(LC_ALL, "zh_CN.UTF-8");
+    MenuItem menu;
 
+    printf("请输入想要添加的菜品ID: ");
+    scanf("%d", &menu.id);
+    printf("请输入想要添加的菜品名字: ");
+    scanf("%s", menu.name);
+    printf("请输入新菜品的价格:");
+    scanf("%lf", &menu.price);
+    printf("请输入新菜品的售卖状态: ");
+    scanf("%d", &menu.status);
+    printf("请输入菜品or酒水: ");
+    scanf("%d", &menu.type);
+
+
+    FILE* file = fopen(filename, "a");
+    if (!file)
+    {
+        printf("无法打开文件 %s。\n", filename);
+        return -1;  // 无法打开文件
+    }
+
+
+        fprintf(file, "%d %s %d %lf %d\n",
+            menu.id,
+            menu.name,
+            menu.type,
+            menu.price,
+            menu.status);
+
+    fprintf(file, "\n");
+    fclose(file);
+
+    printf("添加菜品成功\n");
+    system("pause");
+    return 0;
+
+}
 
